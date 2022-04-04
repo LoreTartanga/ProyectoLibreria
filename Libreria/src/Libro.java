371db6d4f089d4ab970e99c378bf1ba96b367558
¨Lorea¨ <lorea_128@hotmail.com>
2022-04-04 13:24:33 +0200

    Agregados getters y setters de la clase Libro.

--- Libreria/src/Libro.java
+++ Libreria/src/Libro.java
@@ -2,5 +2,32 @@
 public class Libro {
 	private int id;
 	private int cantidad;
+	private boolean leido;
+	private char nif;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	public boolean isLeido() {
+		return leido;
+	}
+	public void setLeido(boolean leido) {
+		this.leido = leido;
+	}
+	public char getNif() {
+		return nif;
+	}
+	public void setNif(char nif) {
+		this.nif = nif;
+	}
+	
 	
 }
